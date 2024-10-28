cc87a5250c02ed8e4c2d1e957f77f92af26f605c
Sarup Hussain <98723561+Sarupsr11@users.noreply.github.com>
2024-10-28 22:44:55 +0100

    Update dblp_testdata.py

--- src/scholaridreconciler/dblp_testdata.py
+++ src/scholaridreconciler/dblp_testdata.py
@@ -1,4 +1,4 @@
-import logging
+port logging
 from SPARQLWrapper import SPARQLWrapper, JSON # type:ignore
 from typing import Any
 from collections.abc import Callable
@@ -29,7 +29,9 @@ def get_url()-> str:
 
 # write the query that need to be addressed
 def query_statement(limit:int, offset:int)-> Any:
-  query= """
+  limit = limit
+  offset = offset
+  query= f"""
   PREFIX datacite: <http://purl.org/spar/datacite/>
   PREFIX dblp: <https://dblp.org/rdf/schema#>
   PREFIX litre: <http://purl.org/spar/literal/>
@@ -57,92 +59,92 @@ def query_statement(limit:int, offset:int)-> Any:
     (GROUP_CONCAT(DISTINCT?ieeeVar;SEPARATOR='|') AS ?ieee) 
     (GROUP_CONCAT(DISTINCT?geprisVar;SEPARATOR='|') AS ?gepris) 
     (GROUP_CONCAT(DISTINCT?gndVar;SEPARATOR='|') AS ?gnd) 
-  WHERE {
+  WHERE {{
     ?editor rdf:type dblp:Person .
     ?editor dblp:primaryCreatorName ?nameVar .
     ?editor datacite:hasIdentifier ?wikidata_blank .
     ?wikidata_blank datacite:usesIdentifierScheme datacite:wikidata ;
                     litre:hasLiteralValue ?wikidataVar .
-    OPTIONAL { ?editor dblp:primaryHomepage ?homepageVar .
-   }
-    OPTIONAL { ?editor dblp:primaryAffiliation ?affiliationVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?dblp_blank .
+    OPTIONAL {{ ?editor dblp:primaryHomepage ?homepageVar .
+   }}
+    OPTIONAL {{ ?editor dblp:primaryAffiliation ?affiliationVar .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?dblp_blank .
       ?dblp_blank datacite:usesIdentifierScheme datacite:dblp ;
                   litre:hasLiteralValue ?dblpVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?orcid_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?orcid_blank .
       ?orcid_blank datacite:usesIdentifierScheme datacite:orcid ;
                    litre:hasLiteralValue ?orcidVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?googleScholar_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?googleScholar_blank .
       ?googleScholar_blank datacite:usesIdentifierScheme datacite:google-scholar ;
                            litre:hasLiteralValue ?googleScholarVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?acm_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?acm_blank .
       ?acm_blank datacite:usesIdentifierScheme datacite:acm ;
                  litre:hasLiteralValue ?acmVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?twitter_blank .
+   }}
+    OPTIONAL {{?editor datacite:hasIdentifier ?twitter_blank .
       ?twitter_blank datacite:usesIdentifierScheme datacite:twitter ;
                      litre:hasLiteralValue ?twitterVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?github_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?github_blank .
       ?github_blank datacite:usesIdentifierScheme datacite:github ;
                     litre:hasLiteralValue ?githubVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?viaf_blank .
+   }}
+    OPTIONAL {{?editor datacite:hasIdentifier ?viaf_blank .
       ?viaf_blank datacite:usesIdentifierScheme datacite:viaf ;
                   litre:hasLiteralValue ?viafVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?scigraph_blank .
+   }}
+    OPTIONAL {{?editor datacite:hasIdentifier ?scigraph_blank .
       ?scigraph_blank datacite:usesIdentifierScheme datacite:scigraph ;
                       litre:hasLiteralValue ?scigraphVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?zbmath_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?zbmath_blank .
       ?zbmath_blank datacite:usesIdentifierScheme datacite:zbmath ;
                     litre:hasLiteralValue ?zbmathVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?researchGate_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?researchGate_blank .
       ?researchGate_blank datacite:usesIdentifierScheme datacite:research-gate ;
                           litre:hasLiteralValue ?researchGateVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?mathGenealogy_blank .
+   }}
+    OPTIONAL {{?editor datacite:hasIdentifier ?mathGenealogy_blank .
       ?mathGenealogy_blank datacite:usesIdentifierScheme datacite:math-genealogy ;
                            litre:hasLiteralValue ?mathGenealogyVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?loc_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?loc_blank .
       ?loc_blank datacite:usesIdentifierScheme datacite:loc ;
                  litre:hasLiteralValue ?locVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?linkedin_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?linkedin_blank .
       ?linkedin_blank datacite:usesIdentifierScheme datacite:linkedin ;
                       litre:hasLiteralValue ?linkedinVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?lattes_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?lattes_blank .
       ?lattes_blank datacite:usesIdentifierScheme datacite:lattes ;
                     litre:hasLiteralValue ?lattesVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?isni_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?isni_blank .
       ?isni_blank datacite:usesIdentifierScheme datacite:isni ;
                   litre:hasLiteralValue ?isniVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?ieee_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?ieee_blank .
       ?ieee_blank datacite:usesIdentifierScheme datacite:ieee ;
                   litre:hasLiteralValue ?ieeeVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?gepris_blank .
+   }}
+    OPTIONAL {{?editor datacite:hasIdentifier ?gepris_blank .
       ?gepris_blank datacite:usesIdentifierScheme datacite:gepris ;
                     litre:hasLiteralValue ?geprisVar .
-   }
-    OPTIONAL { ?editor datacite:hasIdentifier ?gnd_blank .
+   }}
+    OPTIONAL {{ ?editor datacite:hasIdentifier ?gnd_blank .
       ?gnd_blank datacite:usesIdentifierScheme datacite:gnd ;
                  litre:hasLiteralValue ?gndVar .
-   }
-  }
+   }}
+  }}
   GROUP BY ?editor
   LIMIT {limit}
-  OFFSET {ofset}
+  OFFSET {offset}
   """
   return query
 
@@ -167,38 +169,30 @@ def query_call()-> Any:
     return wikidata_author
     
   except Exception as e:
-    logger.error("Error:",e)
+    logger.error(f"Error: {e}")
+    return []
 
 # convert the return result into a dataframe
 def convert_to_dataframe(func: Callable[...,Any]) -> Any:
   wikidata_author = func()
-  n = len(wikidata_author['results']['bindings'])
+  n = len(wikidata_author)
   author_with_wikidata_id ={
-    'name': [wikidata_author['results']['bindings'][i]['name']['value'] for i in range(n)],
-    'homepage': [wikidata_author['results']['bindings'][i]['homepage']['value'] for i in range(n)],
-    'affiliation': [wikidata_author['results']['bindings'][i]['affiliation']['value'] for i in range(n)],
-    'dblp': [wikidata_author['results']['bindings'][i]['dblp']['value'] for i in range(n)],
-    'wikidata': [wikidata_author['results']['bindings'][i]['wikidata']['value'] for i in range(n)],
-    'orcid': [wikidata_author['results']['bindings'][i]['orcid']['value'] for i in range(n)],
-    'googleScholar': [wikidata_author['results']['bindings'][i]['googleScholar']['value'] for i in range(n)],
-    'acm': [wikidata_author['results']['bindings'][i]['acm']['value'] for i in range(n)],
-    'github': [wikidata_author['results']['bindings'][i]['github']['value'] for i in range(n)],
-    'viaf': [wikidata_author['results']['bindings'][i]['viaf']['value'] for i in range(n)],
-    'scigraph': [wikidata_author['results']['bindings'][i]['scigraph']['value'] for i in range(n)],
-    'zbmath': [wikidata_author['results']['bindings'][i]['zbmath']['value'] for i in range(n)],
-    'researchGate': [wikidata_author['results']['bindings'][i]['researchGate']['value'] for i in range(n)]
+    'name': [wikidata_author[i]['name']['value'] for i in range(n)],
+    'homepage': [wikidata_author[i]['homepage']['value'] for i in range(n)],
+    'affiliation': [wikidata_author[i]['affiliation']['value'] for i in range(n)],
+    'dblp': [wikidata_author[i]['dblp']['value'] for i in range(n)],
+    'wikidata': [wikidata_author[i]['wikidata']['value'] for i in range(n)],
+    'orcid': [wikidata_author[i]['orcid']['value'] for i in range(n)],
+    'googleScholar': [wikidata_author[i]['googleScholar']['value'] for i in range(n)],
+    'acm': [wikidata_author[i]['acm']['value'] for i in range(n)],
+    'github': [wikidata_author[i]['github']['value'] for i in range(n)],
+    'viaf': [wikidata_author[i]['viaf']['value'] for i in range(n)],
+    'scigraph': [wikidata_author[i]['scigraph']['value'] for i in range(n)],
+    'zbmath': [wikidata_author[i]['zbmath']['value'] for i in range(n)],
+    'researchGate': [wikidata_author[i]['researchGate']['value'] for i in range(n)]
   }
 
   author_with_wikidata_id_df = pd.DataFrame(author_with_wikidata_id)
   return author_with_wikidata_id_df
 
-Dataframe_id = convert_to_dataframe(query_call())
-logger.info(Dataframe_id)
-
-  
-
-
-    
-
-  
-  
+Dataframe_id = convert_to_dataframe(query_call)
